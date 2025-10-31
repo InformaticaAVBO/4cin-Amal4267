0781d2b9c72e574cbfd9ed6cc9854ca7bbc84ddf
Amin Itrbi <152690464+Amal4267@users.noreply.github.com>
2025-10-31 09:47:11 +0100

    Update App.java

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,10 +1,23 @@
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
-public static void main(String[] args) throws IOException {
+   
+    public void copiaFile(String nomeInput,String nomeOutput) throws Exception {
+    File fileInput = new File(nomeInput);
+    Scanner leggi = new Scanner(fileInput);
+    FileWriter m = new FileWriter(nomeOutput);
+
+
+    while (leggi.hasNextLine()) {
+    String riga = leggi.nextLine();
+    m.write(riga + "\n");
+    leggi.close();
+    m.close();
+}
+}
+public static void main (String[] args) throws Exception {
 Scanner input = new Scanner(System.in);
 
 
@@ -13,24 +26,11 @@ String nomeInput = input.nextLine();
 
 System.out.println("Inserisci il nome del file di output ");
 String nomeOutput = input.nextLine();
-File fileInput = new File(nomeInput);
-
-
-Scanner leggi = new Scanner(fileInput);
-
-FileWriter m = new FileWriter(nomeOutput);
-
-
-while (leggi.hasNextLine()) {
-String riga = leggi.nextLine();
-m.write(riga + "\n");
-}
-
-leggi.close();
-m.close();
-input.close();
-
+App app = new App();
+app.copiaFile(nomeInput, nomeOutput);
 System.out.println("Copia completata! Il contenuto è stato scritto in " + nomeOutput);
+input.close();
 }
 }
+
 
